8d8ddefb73a9d0d25d411f3b9875f36aae16af0d
funkypants3d <stabel@funkypants.net>
2025-08-01 11:33:41 +0200

    backend: - got entry point to work, including PDO and Router - added CORS headers here, need to research how exactly this probably basic thing works - updated DOCKERFILE to include PDO and MySQL, need to get my head around docker - added preflight request check to Router - added constructor to Router to pass in PDO, will see of I have the right idea here.

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,2 +1,37 @@
 <?php
-// bootstrap application in here, also create PDO in here
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Router;
+use App\Http\Request;
+
+// Setup PDO according to GPT, will look more into MySQL in the future
+// Must remember that these credentials are set up in my docker compose file
+// Credentials should obviously be stored securely as SECRETs or in an ENV file, I
+// previously used SECRETs in GCF but we don't have all day here. 
+
+$host = 'mysql';
+$dbname = 'mydb';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+$username = 'user';
+$password = 'password';
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+$pdo = new PDO($dsn, $username, $password, $options);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+$router = new Router($pdo);
+$request = new Request();
+
+$response = $router->dispatch($request);
+
+echo $response;
--- backend/src/Router.php
+++ backend/src/Router.php
@@ -2,25 +2,36 @@
 
 namespace App;
 
-use Nyholm\Psr7\Request;
+use App\Http\Request;
 use EntryController;
+use PDO;
 use UserController;
 
 class Router
 {
+
+    public function __construct(PDO $pdo){}
+
     public function dispatch(Request $request)
     {
-        $method = $request->getMethod();
-        $uri = $request->getUri()->getPath();
+        $method = $request->method;
+        $uri = $request->uri;
+    
+        if ($method === 'OPTIONS') {
+            http_response_code(200);
+            return 'OK';
+        }        
 
         // simple Router layout using if statements to decide which action to call
-
         if ($method === 'GET' && $uri === '/entries') {
             return (new EntryController())->list();
         }
 
         if ($method === 'POST' && $uri === '/entries') {
-            return (new EntryController())->create();
+            return (new EntryController())->create(
+                // need to pass the author and message to this function
+                '', ''
+            );
         }
 
         if ($method === 'POST' && $uri === '/users') {
@@ -35,10 +46,5 @@ class Router
             // call static authentication method in here to keep this part of the Gästebuch simple
             return AuthController::authenticateStatic($request, $pdo);
         }
-
-        $factory = new \Nyholm\Psr7\Factory\Psr17Factory();
-        return $factory->createResponse(404)->withBody(
-            $factory->createStream('404 Not Found')
-        );
     }
 }
