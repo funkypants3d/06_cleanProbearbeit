d76bea41c10db58e3d93e3621f4b238ea662d93d
funkypants3d <stabel@funkypants.net>
2025-08-01 16:40:29 +0200

    backend: updated UserController

--- backend/src/Controllers/UserController.php
+++ backend/src/Controllers/UserController.php
@@ -8,14 +8,22 @@ use App\Models\UserModel;
 class UserController
 {
 
-    public function create(PDO $pdo, string $email, string $password): UserModel
+    public function create(PDO $pdo, string $email, string $password): void
     {
-        $user = new UserModel(['email' => $email, 'password' => $password]);
-        return $user;
+        $stmt = $pdo->prepare("INSERT INTO users (email, password) VALUES (:email :password)");
+        $stmt->execute([
+            'email' => $email, 
+            'password' => $password, 
+        ]);
     }
 
     public function delete(PDO $pdo, int $id): bool
     {
+        // I am not quite sure how to implement this just yet
+        // I know I want the Router to check the request, which should have info about which logged in user made the request, and hand in the according $id. 
+        // I think, maybe I first call the AuthController to find out which authenticated user acutally made the request and return that to the router
+        // which then calls this delete() action?
+        // I will probably sleep over this and save this task for tomorrow. 
         return false;
     }
 }
